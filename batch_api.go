6e3fa7728fb2e5aa7c872f4c46445a53f9d31197
aleksandr.dzikajev <aleksandr.dzikajev@vkg.ee>
2024-03-04 17:12:59 +0200

    batch calculationSummary

--- batch_api.go
+++ batch_api.go
@@ -862,3 +862,130 @@ func (a *BatchApiService) CalculationGetAtTimes(localVarOptionals map[string]int
 	log.Println(batch)
 	return batch, err
 }
+
+/*
+	CalculationApiService Returns the result of evaluating the expression over the time range from the start time to the end time. The time range is first divided into a number of summary intervals. Then the calculation is performed for the specified summaries over each interval.
+
+@param optional (nil or map[string]interface{}) with one or more of:
+
+	@param "calculationBasis" (string) Specifies the method of evaluating the data over the time range. The default is &#39;TimeWeighted&#39;.
+	@param "endTime" (string) An optional end time. The default is &#39;*&#39; for element attributes and points. For event frame attributes, the default is the event frame&#39;s end time, or &#39;*&#39; if that is not set. Note that if endTime is earlier than startTime, the resulting values will be in time-descending order.
+	@param "expression" (string) A string containing the expression to be evaluated. The syntax for the expression generally follows the Performance Equation syntax as described in the PI Server documentation, with the exception that expressions which target AF objects use attribute names in place of tag names in the equation.
+	@param "sampleInterval" (string) A time span specifies how often the filter expression is evaluated when computing the summary for an interval, if the sampleType is &#39;Interval&#39;.
+	@param "sampleType" (string) A flag which specifies one or more summaries to compute for each interval over the time range. The default is &#39;ExpressionRecordedValues&#39;.
+	@param "selectedFields" (string) List of fields to be returned in the response, separated by semicolons (;). If this parameter is not specified, all available fields will be returned.
+	@param "startTime" (string) An optional start time. The default is &#39;*-1d&#39; for element attributes and points. For event frame attributes, the default is the event frame&#39;s start time, or &#39;*-1d&#39; if that is not set.
+	@param "summaryDuration" (string) The duration of each summary interval.
+	@param "summaryType" ([]string) Specifies the kinds of summaries to produce over the range. The default is &#39;Total&#39;. Multiple summary types may be specified by using multiple instances of summaryType.
+	@param "timeType" (string) Specifies how to calculate the timestamp for each interval. The default is &#39;Auto&#39;.
+	@param "webId" (string) The ID of the target object of the expression. A target object can be a Data Server, a database, an element, an event frame or an attribute. References to attributes or points are based on the target. If this parameter is not provided, the target object is set to null.
+
+@return ItemsSummaryValue
+*/
+func (a *BatchApiService) CalculationGetSummary(localVarOptionals map[string]interface{}) (BatchRequest, error) {
+	var (
+		localVarHttpMethod = strings.ToUpper("Get")
+		localVarPostBody   interface{}
+		localVarFileName   string
+		localVarFileBytes  []byte
+		batch              BatchRequest
+	)
+
+	// create path and map variables
+	localVarPath := a.client.cfg.BasePath + "/calculation/summary"
+
+	localVarHeaderParams := make(map[string]string)
+	localVarQueryParams := url.Values{}
+	localVarFormParams := url.Values{}
+
+	if err := typeCheckParameter(localVarOptionals["calculationBasis"], "string", "calculationBasis"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["endTime"], "string", "endTime"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["expression"], "string", "expression"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["sampleInterval"], "string", "sampleInterval"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["sampleType"], "string", "sampleType"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["selectedFields"], "string", "selectedFields"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["startTime"], "string", "startTime"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["summaryDuration"], "string", "summaryDuration"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["timeType"], "string", "timeType"); err != nil {
+		return BatchRequest{}, err
+	}
+	if err := typeCheckParameter(localVarOptionals["webId"], "string", "webId"); err != nil {
+		return BatchRequest{}, err
+	}
+
+	if localVarTempParam, localVarOk := localVarOptionals["calculationBasis"].(string); localVarOk {
+		localVarQueryParams.Add("calculationBasis", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["endTime"].(string); localVarOk {
+		localVarQueryParams.Add("endTime", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["expression"].(string); localVarOk {
+		localVarQueryParams.Add("expression", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["sampleInterval"].(string); localVarOk {
+		localVarQueryParams.Add("sampleInterval", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["sampleType"].(string); localVarOk {
+		localVarQueryParams.Add("sampleType", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["selectedFields"].(string); localVarOk {
+		localVarQueryParams.Add("selectedFields", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["startTime"].(string); localVarOk {
+		localVarQueryParams.Add("startTime", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["summaryDuration"].(string); localVarOk {
+		localVarQueryParams.Add("summaryDuration", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["summaryType"].([]string); localVarOk {
+		for _, t := range localVarTempParam {
+			localVarQueryParams.Add("summaryType", t)
+		}
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["timeType"].(string); localVarOk {
+		localVarQueryParams.Add("timeType", parameterToString(localVarTempParam, ""))
+	}
+	if localVarTempParam, localVarOk := localVarOptionals["webId"].(string); localVarOk {
+		localVarQueryParams.Add("webId", parameterToString(localVarTempParam, ""))
+	}
+
+	r, err := a.client.prepareRequest(
+		a.client.ctx,
+		localVarPath,
+		localVarHttpMethod,
+		localVarPostBody,
+		localVarHeaderParams,
+		localVarQueryParams,
+		localVarFormParams,
+		localVarFileName,
+		localVarFileBytes,
+	)
+	if err != nil {
+		return BatchRequest{}, err
+	}
+
+	//add URL from request to batch
+	batch = BatchRequest{
+		Method:   r.Method,
+		Resource: r.URL.String(),
+	}
+
+	log.Println(batch)
+	return batch, err
+}
